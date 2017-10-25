b62248a333432c13231c904401e8203fef2f5b41
Hugues ALEXANDRE <hugues@111studio.jp>
2017-10-25 16:48:40 +0900

    eslinting

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -6,13 +6,11 @@ import FilesView from '../views/FilesView';
 import LangSelector from '../common/LangSelector';
 
 class Routes extends React.Component {
-  buildRoutes = () => {
-    return [
-      <Route component={EsperView} key={0} path="/espers" />,
-      <Route component={FilesView} key={0} path="/files" />,
-      <Redirect from="/*" key={1} to="/espers" />
-    ];
-  };
+  buildRoutes = () => [
+    <Route component={EsperView} key={0} path="/espers" />,
+    <Route component={FilesView} key={0} path="/files" />,
+    <Redirect from="/*" key={1} to="/espers" />
+  ];
 
   render() {
     const routes = this.buildRoutes();
--- src/stores/Store.js
+++ src/stores/Store.js
@@ -4,5 +4,5 @@ export default class Store {
     this.init(...args);
   }
 
-  init() {}
+  init = () => null;
 }
